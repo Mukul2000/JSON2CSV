521eddf5244894a680dbf1bad871766950a5fc06
Mukul <mukulkumar627@gmail.com>
2021-06-06 16:07:16 +0530

    Add pagination to user files route

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,8 +1,10 @@
 const File = require('../schemas/File');
 
 // find all files which have owner as user_id
-async function get_userfiles(user_id) {
-    const data = await File.find({owner_id: user_id});
+async function get_userfiles(user_id, page_num) {
+    const resPerPage = 15;
+    const page = Math.max(page_num, 1);
+    const data = await File.find({owner_id: user_id}).skip((resPerPage*page) - resPerPage).limit(resPerPage);
     return data;
 }
 
--- server/routes/user.js
+++ server/routes/user.js
@@ -3,8 +3,8 @@ const UserControllers = require('../controllers/user');
 const route = express.Router();
 
 // GET all files belonging to this user_id
-route.get('/:user_id', async (req,res) => {
-    const files = await UserControllers.get_userfiles(req.params.user_id);
+route.get('/:user_id/:page', async (req,res) => {
+    const files = await UserControllers.get_userfiles(req.params.user_id, req.params.page);
     res.send(files);
 });
 
