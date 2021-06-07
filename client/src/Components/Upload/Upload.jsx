bb3a31cb2f3b041271326198a062c989aaea92f8
Mukul <mukulkumar627@gmail.com>
2021-06-07 13:19:50 +0530

    Add upload file constraints

--- client/src/Components/Upload/Upload.jsx
+++ client/src/Components/Upload/Upload.jsx
@@ -8,8 +8,18 @@ export default function Upload() {
     // TODO: Maybe put the POST request code seperately, we'll see.
 
     function handleChange(e) {
-        const file = e.target.files[0];
-        setFile(file);
+        if(e.target.files[0].size > 2097152) {
+            alert("File size must be less than 2MB");
+            e.target.value='';
+        }
+        else if(!e.target.files[0].name.endsWith('.json')) {
+            alert("Only JSON file types allowed");
+            e.target.value='';
+        }
+        else {
+            const file = e.target.files[0];
+            setFile(file);
+        }
     }
 
     async function handleSubmit(e) {
