5789679eb125a877b851825853b7a1ecf7320462
Mukul <mukulkumar627@gmail.com>
2021-06-07 13:33:24 +0530

    Refactor file route and controller

--- server/controllers/file.js
+++ server/controllers/file.js
@@ -1,29 +1,32 @@
 const User = require('../schemas/Schema');
-const {convert_to_CSV, validateJSON} = require('../utils/file_utils');
+const { convert_to_CSV, validateJSON } = require('../utils/file_utils');
 
 // When a file is uploaded to be converted to CSV
 // 2 things have to happen
 // 1. Record in database
 // 2. Convert to CSV and send back
-async function add_convert(file_data, user_data) {
-    const original_filename = file_data['originalname'];
-    const filename = file_data['filename']; // Name inside the server
+async function add_convert(req,res) {
+    const original_filename = req.file.originalname
+    const filename = req.file.filename; // Name inside the server
 
-    const owner_id = user_data['userId'];
-    const name = user_data['userName'];
-    const picture = user_data['userPicture'];
+    const owner_id = req.userId;
+    const name = req.userName;
+    const picture = req.userPicture;
     try {
 
         // Validate, right now it just returns true
-        if(!validateJSON(filename)) throw 'Invalid JSON';
+        if (!validateJSON(filename)) throw 'Invalid JSON';
 
         //Save to DB
-        const user = await User.findOne({id: owner_id});
-        user.files.push({original_filename: original_filename, internal_filename: filename});
+        const user = await User.findOne({ id: owner_id });
+        user.files.push({ original_filename: original_filename, internal_filename: filename });
         Promise.all([user.save(), convert_to_CSV(filename)]);
+        const final_name = req.file.filename + '.csv';
+        res.send(final_name);
     }
     catch (e) {
-        throw e;
+        res.status(400).json({ error: e });
+        console.log(e);
     }
 
 }
--- server/routes/file.js
+++ server/routes/file.js
@@ -7,31 +7,7 @@ const upload = multer({ dest: './uploads/' });
 const path = require('path');
 
 // Upload file to convert to CSV
-route.post('/', authByToken, upload.single('uploaded_json'), async (req, res) => {
-
-    try {
-
-        const user_data = {
-            userId: req.userId,
-            userName: req.userName,
-            userPicture: req.userPicture
-        }
-
-        // req.file.filename holds the name of the file saved in the server
-        await FilesControllers.add_convert(req.file, user_data);
-        const final_name = req.file.filename + '.csv';
-        res.send(final_name);
-    }
-    catch (e) {
-        // res.status(400).json({ error: e });
-        console.log(e);
-    }
-
-
-    // res.sendFile(path.join(__dirname, '../downloads', req.file.filename + '.csv'));
-    // Above shows ENOENT directory does not exist, weird behaviour
-    // Workaround, make the frontend request for this file ?
-});
+route.post('/', authByToken, upload.single('uploaded_json'), FilesControllers.add_convert);
 
 // Get a particular file
 route.get('/:name', (req, res) => {
