115691c5e3bcc13d075f71300d6927dcd276d1aa
Mukul <mukulkumar627@gmail.com>
2021-06-08 11:23:42 +0530

    Switch from local DB to cloud DB

--- server/server.js
+++ server/server.js
@@ -15,11 +15,12 @@ app.use(cors());
 app.use('/files', filesRoute);
 app.use('/user', userRoute);
 
+// TODO: Move this to ENV variables
 const CONNECTION_URL = 'mongodb+srv://adminKC:KarmaCircles@cluster0.tf8rc.mongodb.net/?retryWrites=true&w=majority'
-const CONNECTION_URL1 = 'mongodb://localhost:27017/kcDB';
+const CONNECTION_URL1 = 'mongodb://localhost:27017/kcDB'; // Local test database.
 const PORT = process.env.PORT || 8000;
 
-mongoose.connect(CONNECTION_URL1,  {useNewUrlParser:true, useUnifiedTopology:true, useCreateIndex:true})
+mongoose.connect(CONNECTION_URL,  {useNewUrlParser:true, useUnifiedTopology:true, useCreateIndex:true})
 .then(() => app.listen(8000, () => console.log(`Server started on port ${PORT}`)))
 .catch((err) => console.log(err.message));
 
